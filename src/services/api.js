0f149aa50fcd62461599a7750e2994cbcbc4e399
Lucas Albuquerque <lucas.alb2022@gmail.com>
2025-10-27 21:03:39 -0300

    Update API_BASE_URL for production environment

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,6 @@
 const API_BASE_URL = process.env.NODE_ENV === 'production' 
-  ? 'https://seu-backend-django.onrender.com'  // ← ATUALIZE DEPOIS
+  ? 'https://igreja-backend-yh22.onrender.com'  // ← ATUALIZE DEPOIS
   : 'http://localhost:8000';
+
 
 export default API_BASE_URL;
